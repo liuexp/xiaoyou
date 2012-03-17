608b396442de7d09cd89b01ecbe54bb4c406e8df
Xiao Jia <stfairy@gmail.com>
2012-03-17 17:05:29 +0800

    fill in default settings

---
+++ default.settings.php
@@ -0,0 +1,12 @@
+<?php
+error_reporting(E_ALL & ~E_NOTICE);
+date_default_timezone_set("Asia/Shanghai");
+
+define('DB_NAME', 'xiaoyou');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_HOST', 'localhost');
+
+define('HOST_URL', 'http://localhost');
+define('SITE_BASE', '/xiaoyou');
+define('TITLE_SUFFIX', ' | ACM班校友录');
