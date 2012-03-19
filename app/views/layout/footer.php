283f5150c6c7cc5cc15e6480359449769af8f59f
Xiao Jia <stfairy@gmail.com>
2012-03-19 11:33:33 +0800

    fix footer and logo position

--- app/views/layout/footer.php
+++ app/views/layout/footer.php
@@ -1,7 +1,13 @@
   </div>
   <footer>
     <p>
-      Copyright &copy; 2012. All rights reserved.
+      This website is <a href="https://github.com/oipn4e2/xiaoyou">open source software</a> developed by <a href="http://acm.sjtu.edu.cn/~xjia">Xiao Jia</a>.
+    </p>
+    <p>
+      The ACM class logo was designed by <a href="#">Weinan Zhang</a>. See more <a href="#">credits</a>.
+    </p>
+    <p>
+      Copyright &copy; 2012 <a href="http://acm.sjtu.edu.cn/">ACM class</a>. All rights reserved.
     </p>
     <div class="sponsor">
       <a href="http://www.sjtu.edu.cn/">
