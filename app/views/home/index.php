ca405aa17f533a1dc84a4b653a6114cf6e4f3766
Xiao Jia <stfairy@gmail.com>
2012-03-19 11:06:45 +0800

    fill in lorem ipsum

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -4,40 +4,32 @@ include(__DIR__ . '/../layout/header.php');
 ?>
 
 <section>
-  <p>Redis is an open source, advanced <strong>key-value store</strong>.  It
-  is often referred to as a <strong>data structure server</strong> since
-  keys can contain <a href="/topics/data-types#strings">strings</a>,
-  <a href="/topics/data-types#hashes">hashes</a>, <a href="/topics/data-types#lists">lists</a>,
-  <a href="/topics/data-types#sets">sets</a> and <a href="/topics/data-types#sorted-sets">sorted
-  sets</a>.</p>
-  <p>
-    <a href='/topics/introduction'>Learn more →</a>
-  </p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
   <div class='columns'>
     <section>
-      <h2>Try it</h2>
+      <h2>Lorem ipsum</h2>
       <p>
-        Ready for a test drive? Check this
-        <a href='http://try.redis-db.com'>interactive tutorial</a>
-        that will walk you through the most important features of
-        Redis.
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
     </section>
     <section>
-      <h2>Download it</h2>
+      <h2>Lorem ipsum</h2>
       <p>
-        <a href='http://redis.googlecode.com/files/redis-2.4.9.tar.gz'>Redis 2.4.9 is the latest stable version.</a>
-        Interested in release candidates or unstable versions?
-        <a href='/download'>Check the downloads page.</a>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
     </section>
   </div>
 </section>
 <aside>
   <section data-limit='5' id='buzz'>
-    <h2>What people are saying</h2>
-    <ul></ul>
-    <a href='/buzz'>More...</a>
+    <h2>Lorem ipsum</h2>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    <ul>
+      <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+      <li>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</li>
+    </ul>
   </section>
 </aside>
 
