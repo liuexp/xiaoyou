8d5b9a41c3e86475b536d6bba4e22764484ece4c
Xiao Jia <stfairy@gmail.com>
2012-03-23 19:39:25 +0800

    turn off auto buffering

--- js/video/show.js
+++ js/video/show.js
@@ -2,7 +2,7 @@ $(function(){
   flowplayer('player', window.siteBase + '/swf/flowplayer-3.2.8.swf', {
     clip: {
       autoPlay: false,
-      autoBuffering: true
+      autoBuffering: false
     }
   });
 });
