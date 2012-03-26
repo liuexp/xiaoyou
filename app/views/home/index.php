14013ff4dfb728ecdd5b7cc1eb80ba629b11521a
Xiao Jia <stfairy@gmail.com>
2012-03-26 22:57:35 +0800

    update

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -25,8 +25,8 @@ include(__DIR__ . '/../layout/header.php');
   </section>
 <?php endif; ?>
 <section class="main">
-  <p class="firstpara">　　十年前的3月，上海交通大学夺得ACM国际大学生程序设计竞赛世界冠军，这是亚洲有史以来该赛事的第一个世界冠军，也是交大计算机专业的本科生有史以来第一次站在世界舞台的最高点。</p>
-  <p class="secondpara">　　庆贺之余，促使我们思考更深层的问题：我们的学生是否能够站在世界科学舞台的至高点？由此揭开了上海交通大学计算机科学与技术高端人才培养的序幕⋯⋯<span><a href="<?php echo SITE_BASE; ?>/intro">[全文]</a></span></p>
+  <p class="firstpara">　　十年前的3月，上海交通大学在美国夏威夷夺得ACM国际大学生程序设计竞赛世界冠军，这是亚洲有史以来该赛事的第一个世界冠军，也是上海交通大学计算机专业的本科生有史以来第一次站在世界舞台的最高点。</p>
+  <p class="secondpara">　　庆贺之余，促使我们思考更深层的问题：我们的学生是否能够站在世界科学舞台的至高点？由此揭开了上海交通大学计算机科学与技术高端人才培养的序幕。<span><a href="<?php echo SITE_BASE; ?>/intro">[全文]</a></span></p>
   <div class="columns">
     <section>
       <h2 class="big">新闻</h2>
