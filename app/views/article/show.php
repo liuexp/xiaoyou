2a355b8deda3faa75fe06abd34e87749e3a32649
Xiao Jia <stfairy@gmail.com>
2012-03-28 16:17:09 +0800

    fix

--- app/views/article/show.php
+++ app/views/article/show.php
@@ -4,7 +4,7 @@ $no_sidebar = true;
 $stylesheets = array('article');
 include(__DIR__ . '/../layout/header.php');
 ?>
-<h1><?php echo $title; ?></h1>
+<h1 class="title"><?php echo $title; ?></h1>
 <article><?php echo Markdown($this->article->getContent()); ?></article>
 <?php
 include(__DIR__ . '/../layout/footer.php');
