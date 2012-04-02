00d5bbb00de777a34f4e9055d51d8bb565d4a393
Xiao Jia <stfairy@gmail.com>
2012-04-02 18:26:37 +0800

    limit

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -17,7 +17,7 @@ class HomeController extends ApplicationController
     $this->articles = fRecordSet::build(
       'Article', array('type=' => 'news', 'visible=' => 1), array('priority' => 'desc', 'created_at' => 'desc'), ACTIVITIES_LIMIT);
     $this->posts = fRecordSet::build(
-      'Article', array('type=' => 'post', 'visible=' => 1), array('priority' => 'desc', 'created_at' => 'desc'), ACTIVITIES_LIMIT);
+      'Article', array('type=' => 'post', 'visible=' => 1), array('priority' => 'desc', 'created_at' => 'desc'), ACTIVITIES_LIMIT - 1);
     $this->activities = fRecordSet::build('Activity', array(), array('timestamp' => 'desc'), ACTIVITIES_LIMIT);
     $this->render('home/index');
   }
