dfa73e377417a3dbfc4da05803898320bc4bd51f
Xiao Jia <stfairy@gmail.com>
2012-04-09 11:23:05 +0800

    add tweets timeline

--- app/models/Tweet.php
+++ app/models/Tweet.php
@@ -4,4 +4,9 @@ class Tweet extends fActiveRecord
   protected function configure()
   {
   }
+  
+  public function getProfile()
+  {
+    return new Profile($this->getProfileId());
+  }
 }
--- app/views/tweet/index.php
+++ app/views/tweet/index.php
@@ -1,10 +1,42 @@
 <?php
 $title = '微博';
 $no_sidebar = true;
-$stylesheets = array();
+$stylesheets = array('bootstrap.min', 'tweets');
 include(__DIR__ . '/../layout/header.php');
 ?>
-<h1>最新微博</h1>
+<div class="timeline feed-list">
+  <h1>最新微博</h1>
+<?php foreach ($this->tweets as $tweet): ?>
+  <?php
+    $profile = $tweet->getProfile();
+    $username = $profile->getLoginName();
+    $avatarfile = AVATAR_DIR . $username . '-mini.jpg';
+  ?>
+  <article class="a-feed" id="tweet-<?php echo $tweet->getId(); ?>">
+    <aside>
+      <figure>
+        <a href="<?php echo SITE_BASE; ?>/profile/<?php echo $profile->getId(); ?>">
+          <?php if (file_exists($avatarfile)): ?>
+            <img src="<?php echo AVATAR_BASE; ?>/<?php echo $username; ?>-mini.jpg" width="40px" height="40px"/>
+          <?php else: ?>
+            <img src="<?php echo SITE_BASE; ?>/images/avatar-40.jpg" width="40px" height="40px"/>
+          <?php endif; ?>
+        </a>
+      </figure>
+    </aside>
+    <h3>
+      <a href="<?php echo SITE_BASE; ?>/profile/<?php echo $profile->getId(); ?>"><?php echo $profile->getDisplayName(); ?></a>
+      :
+      <?php echo $tweet->getContent(); ?>
+    </h3>
+    <div class="details">
+      <div class="legend">
+        <span class="duration"><?php echo $tweet->getTimestamp()->getFuzzyDifference(); ?></span>
+      </div>
+    </div>
+  </article>
+<?php endforeach; ?>
+</div>
 <?php
-$javascripts = array('jquery-1.7.1.min');
+$javascripts = array('jquery-1.7.1.min', 'bootstrap.min');
 include(__DIR__ . '/../layout/footer.php');
