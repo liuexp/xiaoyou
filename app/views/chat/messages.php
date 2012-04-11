39db5034d2ac598bf3f1c85f9f45fb9446620bc1
Xiao Jia <stfairy@gmail.com>
2012-04-11 13:04:02 +0800

    refresh to bottom

--- app/views/chat/messages.php
+++ app/views/chat/messages.php
@@ -1,3 +1,9 @@
-<meta http-equiv="refresh" content="<?php echo $this->pollInterval; ?>"/>
+<script type="text/javascript">
+function prepare_refresh() {
+  setTimeout("window.location='#bottom'", <?php echo 1000 * $this->pollInterval; ?>);
+}
+</script>
+<body onload="prepare_refresh()">
 <pre><?php print_r($this->messages); ?></pre>
-<a id="bottom" name="bottom" href="#top">[top]</a>
+<a id="bottom" name="bottom">&nbsp;</a>
+</body>
