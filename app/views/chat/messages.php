39570b3463219ca040bd73e225d2eabbf68a4f5d
Xiao Jia <stfairy@gmail.com>
2012-04-11 13:12:04 +0800

    textarea->input

--- app/views/chat/messages.php
+++ app/views/chat/messages.php
@@ -2,7 +2,7 @@
 <body onload="window.location='#bottom'">
 <pre><?php print_r($this->messages); ?></pre>
 <form action="<?php echo SITE_BASE; ?>/chat" method="post">
-  <textarea name="message"></textarea>
+  <input type="text" name="message" maxlength="140"/>
   <button type="submit">发送</button>
 </form>
 <a id="bottom" name="bottom">&nbsp;</a>
