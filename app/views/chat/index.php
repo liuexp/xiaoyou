a6eb2f37ea3f0043de3b9969296fb1aaa73fc902
Xiao Jia <stfairy@gmail.com>
2012-04-11 13:51:14 +0800

    finish chat views

--- app/views/chat/index.php
+++ app/views/chat/index.php
@@ -4,10 +4,8 @@
   <meta charset="utf-8"/>
   <title>在线聊天</title>
 </head>
-<body>
-  <frameset cols="30%,70%">
-    <frame src="<?php echo SITE_BASE; ?>/chat/users"></frame>
-    <frame src="<?php echo SITE_BASE; ?>/chat/messages#bottom"></frame>
-  </frameset>
-</body>
+<frameset cols="30%,70%">
+  <frame src="<?php echo SITE_BASE; ?>/chat/users"></frame>
+  <frame src="<?php echo SITE_BASE; ?>/chat/messages#bottom"></frame>
+</frameset>
 </html>
