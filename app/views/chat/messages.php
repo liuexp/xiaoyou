145df63da9db9388b30116decf3d37429be1bfd7
Xiao Jia <stfairy@gmail.com>
2012-04-11 13:36:19 +0800

    fix style

--- app/views/chat/messages.php
+++ app/views/chat/messages.php
@@ -42,10 +42,8 @@
       <div class="control-group">
         <label class="control-label" for="message"></label>
         <div class="controls">
-          <div class="input-append">
-            <input class="span4" id="message" type="text" name="message" size="140" maxlength="140"/>
-            <button type="submit" class="btn">发送</button>
-          </div>
+          <input class="span4" id="message" type="text" name="message" size="140" maxlength="140"/>
+          <button type="submit" class="btn btn-primary">发送</button>
         </div>
       </div>
     </fieldset>
