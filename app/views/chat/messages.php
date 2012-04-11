96edd52aeef0f5ff05b692c7a2be0a01f4495866
Xiao Jia <stfairy@gmail.com>
2012-04-11 13:34:06 +0800

    fix

--- app/views/chat/messages.php
+++ app/views/chat/messages.php
@@ -6,6 +6,7 @@
   <link rel="stylesheet" type="text/css" href="<?php echo SITE_BASE; ?>/css/chat.css"/>
 </head>
 <body onload="window.location='#bottom'">
+<div class="container">
   <div class="messages message-list">
   <?php foreach ($this->messages as $message): ?>
     <article class="a-feed">
@@ -35,13 +36,23 @@
     </article>
   <?php endforeach; ?>
   </div>
-  <form action="<?php echo SITE_BASE; ?>/chat" method="post">
-    <input type="text" name="message" size="140" maxlength="140"/>
-    <button type="submit" class="btn btn-primary">发送</button>
+  <form class="form-horizontal" action="<?php echo SITE_BASE; ?>/chat" method="post">
+    <fieldset>
+      <div class="control-group">
+        <label class="control-label" for="message"></label>
+        <div class="controls">
+          <div class="input-append">
+            <input class="span4" id="message" type="text" name="message" size="140" maxlength="140"/>
+            <button type="submit" class="btn">发送</button>
+          </div>
+        </div>
+      </div>
+    </fieldset>
   </form>
   <a id="bottom" name="bottom">&nbsp;</a>
-  <script type="text/javascript" src="<?php echo SITE_BASE; ?>/js/jquery-1.7.1.min.js"></script>
-  <script type="text/javascript" src="<?php echo SITE_BASE; ?>/js/bootstrap.min.js"></script>
-  <script type="text/javascript" src="<?php echo SITE_BASE; ?>/js/hide-broken-images.js"></script>
+</div><!-- /.container -->
+<script type="text/javascript" src="<?php echo SITE_BASE; ?>/js/jquery-1.7.1.min.js"></script>
+<script type="text/javascript" src="<?php echo SITE_BASE; ?>/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="<?php echo SITE_BASE; ?>/js/hide-broken-images.js"></script>
 </body>
 </html>
